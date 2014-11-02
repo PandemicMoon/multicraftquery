8622132a975829b6d8781ea80d60e6c5194f16a8
ThePandemicMoon <pandemic@moon-network.net>
2014-11-01 20:23:57 -0400

    Create Quad.java

---
+++ Quad.java
@@ -0,0 +1,28 @@
+public class Quad {
+  private Object _one;
+  private Object _two;
+  private Object _three;
+  private Object _four;
+  
+  public Quad(Object one, Object two, Object three, Object four) {
+    _one = one;
+    _two = two;
+    _three = three;
+    _four = four;
+  }
+  
+  public Object getOne() {
+    return _one;
+  }
+  
+  public Object getTwo() {
+    return _two;
+  }
+  
+  public Object getThree() {
+    return _three;
+  }
+  public Object getFour() {
+    return _four;
+  }
+}
